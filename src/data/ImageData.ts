be648ba85a22c2c373842b2a8219e614c16fc2a8
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-14 16:43:25 +0530

    Image URL added

---
+++ src/data/ImageData.ts
@@ -0,0 +1,10 @@
+export const ImageData = [
+  'https://cdn.dribbble.com/userupload/38094197/file/original-14de1f5bcaf08193c6658732e62ba742.png?resize=752x&vertical=center',
+  'https://cdn.dribbble.com/userupload/8933057/file/original-e9a10ecd8ac3f89755add6503d14a1f3.png?resize=752x&vertical=center',
+  'https://cdn.dribbble.com/userupload/8999292/file/original-4fa464950ebb3ab0be8869c16d81401d.png?resize=752x&vertical=center',
+  'https://cdn.dribbble.com/userupload/10136479/file/original-75c18e847043c123000d60fabcaece24.png?resize=752x&vertical=center',
+  'https://cdn.dribbble.com/userupload/7698484/file/original-6c199f87f602fc323fad2677693225e6.png?resize=752x&vertical=center',
+  'https://img2.storyblok.com/1220x686/filters:format(webp)/f/102932/1920x1080/156ed23596/monkey-g46eb24a10_1920.jpg',
+  'https://s2.glbimg.com/6fLjMPYo50gB1llNwE11EVXdf8w=/620x430/e.glbimg.com/og/ed/f/original/2022/02/21/https___hypebeast.com_image_2021_10_bored-ape-yacht-club-nft-3-4-million-record-sothebys-metaverse-0.jpg',
+  'https://i0.wp.com/energiainteligenteufjf.com.br/wp-content/uploads/2022/02/bored-ape-yacht-club3-e1643290520341.jpeg?fit=1200%2C638&ssl=1',
+];
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -2,32 +2,12 @@ import React from 'react';
 import { View, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
 
 const DetailScreen = ({ navigation }: { navigation: any }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Details Screen</Text>
-      <Text style={styles.description}>This is the details page!</Text>
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#e0e0e0',
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  description: {
-    fontSize: 16,
-    marginBottom: 20,
-    textAlign: 'center',
   },
 });
 
