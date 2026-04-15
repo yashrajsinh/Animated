ea742ff88f62ee9552b2026fe3104a92c98e6353
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-15 12:56:51 +0530

    bottom scroll visual better

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,12 +1,13 @@
+import { View, Text, FlatList, Image, Dimensions } from 'react-native';
 import React from 'react';
-import { View, StyleSheet, Image, Dimensions } from 'react-native';
-
-//data
+//images URL
 import { ImageData } from '../data/ImageData';
 import Animated, {
   useAnimatedScrollHandler,
   useSharedValue,
 } from 'react-native-reanimated';
+
+type Props = {};
 const { width } = Dimensions.get('screen');
 const _itemSize = width * 0.25;
 const _spacing = 12;
@@ -25,37 +26,28 @@ function Carousel({ imageUri, index }: { imageUri: string; index: number }) {
   );
 }
 
-const DetailScreen = ({ navigation }: { navigation: any }) => {
+const DetailScreen = (props: Props) => {
   const scrollX = useSharedValue(0);
-
   const onScroll = useAnimatedScrollHandler(e => {
-    scrollX.value = e.contentOffset.x;
-    console.log(scrollX.value);
+    scrollX.value = e.contentOffset.x; // show value of X on scroll
   });
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
       <Animated.FlatList
         style={{ flexGrow: 0 }}
+        data={ImageData}
         horizontal
         showsHorizontalScrollIndicator={false}
-        data={ImageData}
         keyExtractor={(_, index) => String(index)}
         renderItem={({ item, index }) => {
           return <Carousel imageUri={item} index={index} />;
         }}
-        //scrolling
+        //scrollling
         onScroll={onScroll}
-        scrollEventThrottle={16} //60 FPS (16 MS)
+        scrollEventThrottle={1000 / 60} //16 ms
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-});
 
 export default DetailScreen;
