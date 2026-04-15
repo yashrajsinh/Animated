f6398440cef39b32b2f782212ee6f33b3e1c3f35
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-15 13:31:03 +0530

    App works fine like a charm

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,50 +1,124 @@
-import { View, Text, FlatList, Image, Dimensions } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+import React, { useState } from 'react';
 //images URL
 import { ImageData } from '../data/ImageData';
 import Animated, {
+  FadeIn,
+  FadeOut,
+  interpolate,
+  interpolateColor,
+  SharedValue,
   useAnimatedScrollHandler,
+  useAnimatedStyle,
   useSharedValue,
 } from 'react-native-reanimated';
+import { runOnJS } from 'react-native-worklets';
 
 type Props = {};
 const { width } = Dimensions.get('screen');
 const _itemSize = width * 0.25;
 const _spacing = 12;
-function Carousel({ imageUri, index }: { imageUri: string; index: number }) {
+const _itemTotalSize = _itemSize + _spacing;
+
+function Carousel({
+  imageUri,
+  index,
+  scrollX,
+}: {
+  imageUri: string;
+  index: number;
+  scrollX: SharedValue<number>;
+}) {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      borderWidth: 4,
+      borderColor: interpolateColor(
+        scrollX.value,
+        [index - 1, index, index + 1],
+        ['transparent', 'white', 'transparent'],
+      ),
+      transform: [
+        {
+          translateY: interpolate(
+            scrollX.value,
+            [index - 1, index, index + 1],
+            [_itemSize / 3, 0, _itemSize / 3],
+          ),
+        },
+      ],
+    };
+  });
   return (
-    <View>
-      <Image
-        source={{ uri: imageUri }}
-        style={{
+    <Animated.View
+      style={[
+        {
           width: _itemSize,
           height: _itemSize,
           borderRadius: _itemSize / 2,
+        },
+        stylez,
+      ]}
+    >
+      <Image
+        source={{ uri: imageUri }}
+        style={{
+          flex: 1,
+          borderRadius: _itemSize / 2,
         }}
       />
-    </View>
+    </Animated.View>
   );
 }
 
 const DetailScreen = (props: Props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const scrollX = useSharedValue(0);
   const onScroll = useAnimatedScrollHandler(e => {
-    scrollX.value = e.contentOffset.x; // show value of X on scroll
+    scrollX.value = e.contentOffset.x / _itemTotalSize; // show value of X on scroll
+    //round up index
+    const newActiveIndex = Math.round(scrollX.value);
+    if (activeIndex !== newActiveIndex) {
+      runOnJS(setActiveIndex)(newActiveIndex);
+    }
   });
   return (
-    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+    <View
+      style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: '#000' }}
+    >
+      <View style={StyleSheet.absoluteFill}>
+        <Animated.Image
+          entering={FadeIn.duration(500)}
+          exiting={FadeOut.duration(500)}
+          key={`imgae-${activeIndex}`}
+          style={{ flex: 1 }}
+          source={{ uri: ImageData[activeIndex] }}
+        />
+      </View>
       <Animated.FlatList
-        style={{ flexGrow: 0 }}
+        style={{ flexGrow: 0, height: _itemSize * 2 }}
         data={ImageData}
         horizontal
         showsHorizontalScrollIndicator={false}
         keyExtractor={(_, index) => String(index)}
         renderItem={({ item, index }) => {
-          return <Carousel imageUri={item} index={index} />;
+          return <Carousel imageUri={item} index={index} scrollX={scrollX} />;
         }}
         //scrollling
         onScroll={onScroll}
         scrollEventThrottle={1000 / 60} //16 ms
+        contentContainerStyle={{
+          paddingHorizontal: (width - _itemSize) / 2,
+          gap: _spacing,
+        }}
+        snapToInterval={_itemTotalSize}
+        decelerationRate={'fast'}
       />
     </View>
   );
